c95beeb6998aeb08a3e21962d391568671f684be
AquaTim <tim.suppiger@aquatime.ch>
2020-01-17 17:27:53 +0100

    Projekt Erstellt

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AquaKidz</title>
+
+    <!-- Styles -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet"  href="vendor/bootstrap/bootstrap.min.css" type="text/css">
+    <!-- Fontawesome -->
+    <link href="vendor/fontawesome-free-5.11.2-web/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <!-----Eigene Styles----->
+    <!-- Style (Hauptstyle) -->
+    <link rel="stylesheet" href="css/style.scss">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="Index">
+
+            AquaTime GmbH
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a href="Index#AquaKidz" class="nav-link smoothScroll">AquaKidz</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Index#AquaKidz" class="nav-link smoothScroll">AquaFit4You</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Index#AquaTherapeutics" class="nav-link">AquaTherapeutics</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Kontakt" class="nav-link contact">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<header>
+    <img src="logo.svg" alt="logo">
+    <h1>Überschrift</h1>
+</header>
+
+
+<main>
+
+</main>
+
+
+<footer>
+    <a href="kontakt.html">Kontakt</a>
+    <p>©AquaTime GmbH 2015 - <?php echo date(Y)?></p>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
