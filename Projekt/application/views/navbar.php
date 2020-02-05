6d7432f366e93f2b7c0d7f4ee212d750beea6f4b
Djordje0007 <45971835+Djordje0007@users.noreply.github.com>
2020-02-05 18:10:13 +0100

    code formatted
    -responsive navbar
    -css cleaning
    -button (cards)
    -grid

--- Projekt/application/views/navbar.php
+++ Projekt/application/views/navbar.php
@@ -1,28 +1,52 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary" style="height:80px;">
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-primary" style="height:80px;">
   <div class="container">
-    <div class="logo">
-     <a class="navbar-brand"><img src="../../assets/images/Logo.png" class="card-img-top"  alt="Bild ladet nicht!"></a>
-    </div>
-    
-    <ul class="navbar-nav">   
-      <li class="nav-item">
-          <a class="nav-link" href="#">Schwimmkurse</a>
-      </li>
-      <li class="nav-item  my-2 my-sm-0">
-          <a class="nav-link" href="#">Kursort</a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink">Über uns</a>
-      </li>
+    <!--Der (Burger) wenn die seite kleiner wird-->
+      <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+    <div class="collapse navbar-collapse" id="collapse_target">
+      <div class="col-1">
+        <div class="logo">
+          <img src="../../assets/images/Logo.png" class="card-img-top"  alt="Bild ladet nicht!">
+        </div>
+      </div>
+
+    <div class="col-4"></div>
+    <div class="col-7">
+    <ul class="navbar-nav"> 
+   
+        <li class="nav-item">
+            <a class="nav-link" href="#">Schwimmkurse</a>
+        </li>
+      
+        <li class="nav-item">
+            <a class="nav-link" href="#">Kursort</a>
+        </li>
+      
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" data-target="dropdown_target">Über uns</a>
+            <div class="dropdown-menu" aria-labelledby="dropdowntarget">
+            <a class="dropdown-item"> dropdown 1</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item"> dropdown 2</a>
+            <a class="dropdown-item"> dropdown 3</a>
+          
+        </li>
+      </ul>
+      
       <li class="nav-item ">
           <a class="nav-link" href="#">Kontakt</a>
       </li>
       
       <li class="nav-item">
-        <div class="nav-link btn btn-primary" style="border-radius:40px; border-color:white; width:100px; height:auto" >Login</div>
+        <div class="nav-link btn btn-primary" style="border-radius:40px; border-color:white; width:100px; height:37px" >Login</div>
       </li>
-
-    </ul>   
-   
+      
+      </ul>  
+      </div> 
+    </div>
   </div>
 </nav>
+<body>
