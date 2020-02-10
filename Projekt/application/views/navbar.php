0d2bfb89b63a5d3c22157caf1b9c5c3ebc95b1f7
AquaTim <tim.suppiger@aquatime.ch>
2020-02-10 17:47:51 +0100

    .

--- Projekt/application/views/navbar.php
+++ Projekt/application/views/navbar.php
@@ -1,8 +1,76 @@
 <body>
-  <div class="aqua-navbar" style="">
+
+<style>
+    .testnav{
+        margin-left: auto;
+        margin-right: auto;
+        max-width: 1336px;
+        background-color: red;
+        height: 100%;
+    }
+
+    .testnav2{
+        height: 80px;
+        width: 100%;
+        background-color: var(--maincolor);
+        border-bottom: 5px solid white;
+        z-index: 1000;
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+
+    .testnav li{
+        display: inline !important;
+        list-style-type: none !important;
+
+    }
+
+
+
+
+
+</style>
+<section class="testnav2">
+<nav class="testnav">
+    <section>
+        <ul>
+            <li>
+                <a href=”#”>SCHWIMMKURSE</a>
+            </li>
+            <li>
+                <a href=”#”>KURSORT</a>
+            </li>
+            <li>
+                <a href=”#”>ÜBER UNS</a>
+            </li>
+            <li>
+                <a href=”#”>KONTAKT</a>
+            </li>
+        </ul>
+
+    </section>
+
+
+
+
+
+
+
+
+
+</nav>
+</section>
+
+
+
+
+
+<!--  <div class="aqua-navbar" style="">
         <nav class="navbar sticky-top navbar-expand-md navbar-dark " style="height:80px; background-color:var(--maincolor);">
     
-    <!--Der (Burger) wenn die seite kleiner wird-->
+   Der (Burger) wenn die seite kleiner wird-
       <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -19,7 +87,7 @@
    
     <ul class="navbar-nav" style="float:right;"> 
         <li class="nav-item ">
-            <a class="nav-link " href="<?= base_url();?>Homepage_con/schwimmkurse" target="_blank" style="border-radius:20px 0px 0px 0px;  background-color: white; color:#416cb2;">SCHWIMMKURSE</a>
+            <a class="nav-link " href="<?/*= base_url();*/?>Homepage_con/schwimmkurse" target="_blank" style="border-radius:20px 0px 0px 0px;  background-color: white; color:#416cb2;">SCHWIMMKURSE</a>
         </li>
       
         <li class="nav-item">
@@ -28,7 +96,7 @@
       
       <ul class="navbar-nav">
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="<?= base_url();?>Homepage_con/sites" target="_blank" data-toggle="dropdown" data-target="dropdown_target" style="background-color:white; color:#416cb2;">ÜBER UNS</a>
+          <a class="nav-link dropdown-toggle" href="<?/*= base_url();*/?>Homepage_con/sites" target="_blank" data-toggle="dropdown" data-target="dropdown_target" style="background-color:white; color:#416cb2;">ÜBER UNS</a>
             <div class="dropdown-menu" aria-labelledby="dropdowntarget">
             <a class="dropdown-item"> dropdown 1</a>
             <div class="dropdown-divider"></div>
@@ -46,10 +114,10 @@
        
       
       </ul>  
-      <!--<a class="btn" href="#" style="color:white;">LOGIN</a>-->
+      <a class="btn" href="#" style="color:white;">LOGIN</a>
       </div> 
 
 
 
   </nav>
-</div>
+</div>-->
