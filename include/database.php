e3365e73bf537c31bb7b65bbf3cf3511113c0a53
Djordje0007 <45971835+Djordje0007@users.noreply.github.com>
2020-02-20 12:06:29 +0100

    database

---
+++ include/database.php
@@ -0,0 +1,30 @@
+<?php
+//DATENBANK//
+
+//HOST
+$db_host = "localhost";
+
+//USER
+$db_user = "root";
+
+//PASSWORT
+$db_password = "root";
+
+//DATENBANK
+$db_database = "AquaTime-Neu";
+
+
+///////////
+//VERSION 1
+///////////
+$con_mysqli_connect = mysqli_connect("$db_host", "$db_user", "$db_password", "$db_database");
+
+///////////
+//VERSION 2
+///////////
+$pdo_PDO = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_database . '', "$db_user", "$db_password");
+
+///////////
+//VERSION 3
+///////////
+$con_mysqli = new mysqli($db_host, $db_user, $db_password, $db_database);
