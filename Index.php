59d9ff89867e44b10dfbce2dc693362fb144cf6d
Djordje0007 <45971835+Djordje0007@users.noreply.github.com>
2020-03-21 00:11:58 +0100

    .

--- Index.php
+++ Index.php
@@ -24,6 +24,8 @@ include  $url_helper .'include/database.php';
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="<?php echo $url_helper;?>css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Lato&display=swap" rel="stylesheet">
+      <!-- Custom fonts for this template-->
+    <link href="fonts/css/all.min.css" rel="stylesheet" type="text/css">
 
 <!-- Required meta tags -->    <!--einbindung der cookie.js datei-->
     <script type="text/javascript" src="cookie.js"></script>
--- include/footer.php
+++ include/footer.php
@@ -154,13 +154,84 @@
         </div>        
         <ok>
         
-            <div id="overlay" onclick="off()">
-                <div class="show-box-details" id="text"></div>
-            </div>
+         
     <div class="row" class="pos">
         <div class="col-4">
-            <button class="btn btn_details" onclick="on()">Details</button>
+            <button class="btn btn_details" id="myBtn" >Details</button>
     </div>
+
+
+
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+
+        
+        <button type="button" class="collapsible">Geltungsbereich</button>
+        <div class="content">
+        <p>Diese Datenschutzerklärung soll die Nutzer dieser Website gemäß europäschischer Datenschutzgrundverordnung (DSGVO) über die Art, den Umfang und den Zweck der Erhebung und Verwendung personenbezogener Daten durch den Websitebetreiber Sebastian Prohaska informieren.
+Der Websitebetreiber nimmt Ihren Datenschutz sehr ernst und behandelt Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Vorschriften.
+Bedenken Sie, dass die Datenübertragung im Internet grundsätzlich mit Sicherheitslücken bedacht sein kann. Ein vollumfänglicher Schutz vor dem Zugriff durch Fremde ist nicht realisierbar.</p>
+        </div>
+
+        <button type="button" class="collapsible">Zugriffsdaten</button>
+        <div class="content">
+        <p>Der Websitebetreiber bzw. Seitenprovider erhebt Daten über Zugriffe auf die Seite und speichert diese als „Server-Logfiles“ ab. Folgende Daten werden so protokolliert:
+            <ul>
+<li>Besuchte Website</li> 
+ <li>Uhrzeit zum Zeitpunkt des Zugriffes</li> 
+<li>Menge der gesendeten Daten in Byte</li> 
+<li>Quelle/Verweis, von welchem Sie auf die Seite gelangten</li> 
+<li>Verwendeter Browser</li> 
+<li>Verwendetes Betriebssystem</li> 
+<li>Verwendete IP-Adresse</li> Die erhobenen Daten dienen lediglich statistischen Auswertungen und zur Verbesserung der Website. Der Websitebetreiber behält sich allerdings vor, die Server-Logfiles nachträglich zu überprüfen, sollten konkrete Anhaltspunkte auf eine rechtswidrige Nutzung hinweisen.
+</ul>
+</p>
+        </div>
+        <button type="button" class="collapsible">Cookies</button>
+        <div class="content">
+        <p>Diese Website verwendet Cookies. Dabei handelt es sich um kleine Textdateien, welche auf Ihrem Endgerät gespeichert werden. Diese richten keinerlei Schaden an. Ihr Browser greift auf diese Dateien zu. Durch den Einsatz von Cookies erhöht sich die Benutzerfreundlichkeit und Sicherheit dieser Website.
+Gängige Browser bieten die Einstellungsoption, Cookies nicht zuzulassen. Hinweis: Es ist nicht gewährleistet, dass Sie auf alle Funktionen dieser Website ohne Einschränkungen zugreifen können, wenn Sie entsprechende Einstellungen vornehmen.</p>
+        </div>
+        <button type="button" class="collapsible">Umgang mit personenbezogenen Daten</button>
+        <div class="content">
+        <p>Der Websitebetreiber erhebt, nutzt und gibt Ihre personenbezogenen Daten nur dann weiter, wenn dies im gesetzlichen Rahmen erlaubt ist oder Sie in die Datenerhebung einwilligen.
+Als personenbezogene Daten gelten sämtliche Informationen, welche dazu dienen, Ihre Person zu bestimmen und welche zu Ihnen zurückverfolgt werden können – also beispielsweise Ihr Name, Ihre E-Mail-Adresse und Telefonnummer.
+
+</p>
+        </div>
+          <button type="button" class="collapsible">Google Analytics</button>
+        <div class="content">
+        <p style="overflow:scroll;">Diese Website nutzt den Dienst „Google Analytics“, welcher von der Google Inc. (1600 Amphitheatre Parkway Mountain View, CA 94043, USA) angeboten wird, zur Analyse der Websitebenutzung durch Nutzer. Der Dienst verwendet „Cookies“ – Textdateien, welche auf Ihrem Endgerät gespeichert werden. Die durch die Cookies gesammelten Informationen werden im Regelfall an einen Google-Server in den USA gesandt und dort gespeichert.
+
+Auf dieser Website greift die IP-Anonymisierung. Die IP-Adresse der Nutzer wird innerhalb der Mitgliedsstaaten der EU und des Europäischen Wirtschaftsraum gekürzt. Durch diese Kürzung entfällt der Personenbezug Ihrer IP-Adresse. Im Rahmen der Vereinbarung zur Auftragsdatenvereinbarung, welche die Websitebetreiber mit der Google Inc. geschlossen haben, erstellt diese mithilfe der gesammelten Informationen eine Auswertung der Websitenutzung und der Websiteaktivität und erbringt mit der Internetnutzung verbundene Dienstleistungen.
+Sie haben die Möglichkeit, die Speicherung des Cookies auf Ihrem Gerät zu verhindern, indem Sie in Ihrem Browser entsprechende Einstellungen vornehmen. Es ist nicht gewährleistet, dass Sie auf alle Funktionen dieser Website ohne Einschränkungen zugreifen können, wenn Ihr Browser keine Cookies zulässt.
+
+Weiterhin können Sie durch ein Browser-Plugin verhindern, dass die durch Cookies gesammelten Informationen (inklusive Ihrer IP-Adresse) an die Google Inc. gesendet und von der Google Inc. genutzt werden. Folgender Link führt Sie zu dem entsprechenden Plugin: https://tools.google.com/dlpage/gaoptout?hl=de
+
+Alternativ verhindern Sie mit einem Klick auf diesen Link (WICHTIG! Opt-Out-Link einfügen), dass Google Analytics innerhalb dieser Website Daten über Sie erfasst. Mit dem Klick auf obigen Link laden Sie ein „Opt-Out-Cookie“ herunter. Ihr Browser muss die Speicherung von Cookies also hierzu grundsätzlich erlauben. Löschen Sie Ihre Cookies regelmäßig, ist ein erneuter Klick auf den Link bei jedem Besuch dieser Website vonnöten.
+Hier finden Sie weitere Informationen zur Datennutzung durch die Google Inc.: https://support.google.com/analytics/answer/6004245?hl=de</p>
+        </div>
+          <button type="button" class="collapsible">Newsletter-Abonnement</button>
+        <div class="content">
+        <p>Der Websitebetreiber bietet Ihnen einen Newsletter an, in welchem er Sie über aktuelle Geschehnisse und Angebote informiert. Möchten Sie den Newsletter abonnieren, müssen Sie eine valide E-Mail-Adresse angeben. Diese wird lediglich für den Versand des abonnierten Newsletters verwendet und nicht an Dritte weitergereicht. Sie können das Abonnement jederzeit abbestellen.</p>
+        </div>
+           <button type="button" class="collapsible">Rechte des Nutzers: Auskunft, Berichtigung und Löschung</button>
+        <div class="content">
+        <p>Sie als Nutzer erhalten auf Antrag Ihrerseits kostenlose Auskunft darüber, welche personenbezogenen Daten über Sie gespeichert wurden. Sofern Ihr Wunsch nicht mit einer gesetzlichen Pflicht zur Aufbewahrung von Daten (z. B. Vorratsdatenspeicherung) kollidiert, haben Sie ein Anrecht auf Berichtigung falscher Daten und auf die Sperrung oder Löschung Ihrer personenbezogenen Daten.
+
+Diese Datenschutzerklärung wurde von ithelps - SEO Agentur und Webdesign Wien rechtmäßig kopiert.</p>
+        </div>
+ </div>
+        </div>
+
+
+
+
+
 
      <form method="post" >
         <span class="more">
@@ -217,19 +288,51 @@ if(r == '1') document.getElementById('cookie_popup').style.display = 'none';
 
 <!--Detais script beginn-->
 <script>
-function on() {
-  document.getElementById("overlay").style.display = "block";
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function off() {
-  document.getElementById("overlay").style.display = "none";
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 </script>
 <!--Detais script ende-->
 
 
+<script>
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
 
 
 
