d63ab0ec2ff69bc5f48a97997ab4f5a16644b91e
Djordje0007 <45971835+Djordje0007@users.noreply.github.com>
2020-07-22 09:28:54 +0200

    .

--- include/navbar.php
+++ include/navbar.php
@@ -1,70 +1,61 @@
-<!--Navbar hidden-->
-
-<div class="aqua-hidden"></div>
-
-
 <!--Navbar-->
-<div class="aqua-navbar">
-  <div class="container">
-    <nav class="navbar navbar-expand-md navbar-dark" style="height:88px;">
+  <div class="aqua-navbar">
+    <div class="container">
+      <nav class="navbar navbar-expand-md" style="height:88px;">
+      
+      <div class="logo navbar-brand">
+        <a href="<?php echo $url_helper;?>index">
+          <img src="<?php echo $url_helper;?>images/Logo.png" class="card-img-top" alt="Bild ladet nicht!">
+        </a>
+      </div>
 
-    
 
       
-        <div class="col-2">
-          <div class="logo">
-            <a href="<?php echo $url_helper;?>index">
-              <logo-navbar>
-                <img src="<?php echo $url_helper;?>images/Logo.png" class="card-img-top" alt="Bild ladet nicht!">
-              </logo-navbar>
-            </a>
-          </div>
-        </div>
-          <!-- Collapse button -->
-      <button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
-        <span class="navbar-toggler-icon"></span>
+      <!-- Collapse button -->
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target"
+        aria-controls="collapse_target" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars fa-1x "></i>
       </button>
 
-    <!-- Collapsible content -->
-    <div class="collapse navbar-collapse" id="collapse_target">
-       
-        <div class="navbar-pos">
+      <!-- Collapsible content -->
+      <div class="collapse navbar-collapse" id="collapse_target">
 
-      
-          <ul class="navbar-nav " style="float:right;">
-            <li class="nav-item ">
-              <a class="nav-link <?= $aktivschwimmkurse; ?>" href="<?php echo $url_helper;?>Schwimmkurse"
-                style="border-radius:20px 0px 0px 0px;  background-color: white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">SCHWIMMKURSE</a>
-            </li>
+        <ul class="nav navbar-nav align-items-end ml-auto">
+          <li class="nav-item ">
+            <a class="nav-link <?= $aktivschwimmkurse; ?>" href="<?php echo $url_helper;?>Schwimmkurse"
+              style="border-radius:20px 0px 0px 0px;  background-color: white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">SCHWIMMKURSE</a>
+          </li>
 
 
 
-            <li class="nav-item ">
-              <a class="nav-link <?= $aktivkursort;  ?>" href="<?php echo $url_helper;?>Kursorte"
-                style="background-color:white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">KURSORT</a>
-            </li>
+          <li class="nav-item ">
+            <a class="nav-link <?= $aktivkursort;  ?>" href="<?php echo $url_helper;?>Kursorte"
+              style="background-color:white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">KURSORT</a>
+          </li>
 
 
-            <li class="nav-item ">
-              <a class="nav-link <?= $aktivueberuns;  ?>" href="<?php echo $url_helper;?>Ueber_uns"
-                style="background-color:white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">ÜBER UNS</i></a>
-            </li>
+          <li class="nav-item ">
+            <a class="nav-link <?= $aktivueberuns;  ?>" href="<?php echo $url_helper;?>Ueber_uns"
+              style="background-color:white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">ÜBER UNS</i></a>
+          </li>
 
 
-            <li class="nav-item ">
-              <a class="nav-link <?= $aktivkontakt; ?>" href="<?php echo $url_helper;?>Kontakt"
-                style="border-radius:0px 20px 0px 0px; background-color:white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">KONTAKT</a>
-            </li>
+          <li class="nav-item ">
+            <a class="nav-link <?= $aktivkontakt; ?>" href="<?php echo $url_helper;?>Kontakt"
+              style="border-radius:0px 20px 0px 0px; background-color:white; color:#416cb2; font-family: 'Fjalla One', sans-serif;">KONTAKT</a>
+          </li>
 
-          </ul>
+        </ul>
 
-          <div class="login-pos">
-            <a class="btn" href="<?php echo $url_helper;?>Index" target="_blank"
-              style="color:white; font-family: 'Fjalla One', sans-serif;">LOGIN</a>
-          </div>
-        </div>
 
+          <a class="btn" href="<?php echo $url_helper;?>Index" target="_blank"
+            style="color:white; font-family: 'Fjalla One', sans-serif;">LOGIN</a>
+ 
       </div>
-    </nav>
+
+
+
+
+    </div>
   </div>
-</div>
+</nav>
